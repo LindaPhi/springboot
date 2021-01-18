24c28d7e796a264a1e0020c74a94ff58a90cfa92
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2021-01-18 14:47:13 +0100

    first commit

---
+++ src/main/java/com/msa/don_and_dra/DonAndDraApplication.java
@@ -0,0 +1,13 @@
+package com.msa.don_and_dra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonAndDraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonAndDraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msa/don_and_dra/DonAndDraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.don_and_dra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonAndDraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
